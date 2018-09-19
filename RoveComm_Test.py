9914cfce237c9ba33a8ae469f312326272f4b716
andrewvh4 <31864022+andrewvh4@users.noreply.github.com>
2018-09-19 00:11:15 -0500

    Created RoveComm 2.0 Proposal

--- RoveComm_Test.py
+++ RoveComm_Test.py
@@ -1,8 +1,17 @@
-from RoveComm_Python import RoveCommEthernetUdp
+python
+from RoveComm_Python import *
 import struct
 local_host_ip = "127.0.0.1"
+packet = RoveCommPacket(10, 'b', (3, 4, 5, 6))
+packet2 = RoveCommPacket()
+packet.SetIp(local_host_ip)
+packet.print()
 RoveComm = RoveCommEthernetUdp()
-RoveComm.writeTo(10, 11, struct.pack('L', 7), local_host_ip)
+RoveComm.write(packet)
+packet2 = RoveComm.read()
+packet2.print()
+quit()
+
 data_id, data_size, data = RoveComm.read()
 
 print(data_id, data_size, struct.unpack('L', data))
@@ -14,3 +23,5 @@ print(RoveComm.subscribers)
 
 RoveComm.writeTo(10, 11, struct.pack('L', 7), local_host_ip)
 
+conda activate RoveComm_Python
+cd C:\Users\andre\Documents\Rover\GitHub\Libraries\RoveComm_Python
